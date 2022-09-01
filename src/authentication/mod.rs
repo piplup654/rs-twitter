cc04c2f2656fa7887d04fab29a18a63009112bb9
Synth <piplup654@protonmail.com>
2022-09-01 15:03:54 +0200

    Refactored authentication error handling

--- src/authentication/mod.rs
+++ src/authentication/mod.rs
@@ -1,7 +1,6 @@
 use base64;
 use dotenv::dotenv;
 pub mod helper_structs;
-
 const AUTHORIZE_ENDPOINT: &str = "https://api.twitter.com/oauth2/token";
 
 pub struct Client {
@@ -18,9 +17,14 @@ impl std::fmt::Display for Client {
 
 pub fn get_api_credentials() -> Result<(String, String), Box<dyn std::error::Error>> {
     dotenv().ok();
-    // implement better error support with match and stuff
-    let api_key = std::env::var("API_KEY")?;
-    let api_secret = std::env::var("API_SECRET")?;
+    let api_key = match std::env::var("API_KEY") {
+        Ok(api_key) => api_key,
+        Err(_) => panic!("No .env file found or no API_KEY field specified"),
+    };
+    let api_secret = match std::env::var("API_SECRET") {
+        Ok(api_secret) => api_secret,
+        Err(_) => panic!("No .env file found or no API_SECRET field specified"),
+    };
     Ok((api_key, api_secret))
 }
 
@@ -35,25 +39,28 @@ impl Client {
             grant_type: "client_credentials".to_string()
         };
         let auth_body_encoded = serde_urlencoded::to_string(&auth_body).unwrap();
-        let auth_request = reqwest::Client::new().post(AUTHORIZE_ENDPOINT)
+        let auth_request = match reqwest::Client::new().post(AUTHORIZE_ENDPOINT)
             .header("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
             .header("Authorization", format!("Basic {}", client_string))
             .body(auth_body_encoded)
             .send()
             .await
-            .unwrap().json::<helper_structs::AuthResponse>().await.unwrap();
+            .unwrap().json::<helper_structs::AuthResponse>().await {
+                Ok(resp) => resp,
+                Err(_) => panic!("Error while trying to request bearer token")
+            };
         let bearer: String = auth_request.access_token;
         self.bearer_token = bearer;
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::{self, Rng};
     #[test]
     fn check_client_creation() {
-        let api_key = String::from("123");
-        let api_secret = String::from("543");
+        let api_key = String::from(rand::thread_rng().gen_range(2312323..3232313).to_string());
+        let api_secret = String::from(rand::thread_rng().gen_range(2312323..3232313).to_string());
         let client: Client = Client::new(api_key.clone(), api_secret.clone());
         assert_eq!(client.api_key, api_key);
     }
