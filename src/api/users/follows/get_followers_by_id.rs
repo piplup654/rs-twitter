9d4752428d4bea3edd722940476184a6bfbdb3eb
Synth <piplup654@protonmail.com>
2022-09-02 16:56:46 +0200

    Implemented full get_followers_by_id endpoint excluding token_paginator

--- src/api/users/follows/get_followers_by_id.rs
+++ src/api/users/follows/get_followers_by_id.rs
@@ -1,17 +1,46 @@
+use std::{collections::HashMap, fmt::Error};
 use crate::authentication::Client;
 use serde_json::value::Value;
-use super::params_structs::{PGetFollowersById, PGetFollowersByIdBuilder};
+use super::params_structs::{PGetFollowersById, PGetFollowersByIdBuilder, QPGetFollowersById};
 
 impl Client {
-    pub async fn get_followers_by_id(&self, params: &PGetFollowersById) -> Result<Value, Box<dyn std::error::Error>> {
+    pub async fn get_followers_by_id(&self, params: &PGetFollowersById) -> Result<Value, reqwest::Error> {
         let bearer_header = format!("Bearer {}", &self.bearer_token[..]);
-        let reqwest_client = reqwest::Client::new();
         let url = format!("https://api.twitter.com/2/users/{}/followers", params.id);
-        let user_request = reqwest_client.get(url)
+        let reqwest_client = reqwest::Client::new();
+        // optional parameters handling
+        let empty_vec: Vec<String> = vec![String::from("")];
+
+        let expansions = params.expansions.as_ref().unwrap_or_else(|| &empty_vec);
+
+        let tweet_fields = params.tweet_fields.as_ref().unwrap_or_else(|| &empty_vec);
+
+        let user_fields = params.user_fields.as_ref().unwrap_or_else(|| &empty_vec);
+
+        let max_results = &params.max_results.unwrap_or_else(|| 1);
+
+        // TODO fix pagination token
+        // let pagination_token = params.pagination_token.as_ref().unwrap_or_else(|| &empty_string);
+        // ----------------------------
+
+        let query_params = QPGetFollowersById {
+            max_results: *max_results,
+            expansions: expansions.join(","),
+        };
+
+        let mut query_params_2: HashMap<String, String> = HashMap::new();
+        query_params_2.insert("user.fields".to_string(), user_fields.join(","));
+        query_params_2.insert("tweet.fields".to_string(), tweet_fields.join(","));
+
+        let user_request = match reqwest_client.get(url)
             .header(reqwest::header::AUTHORIZATION, bearer_header)
+            .query(&query_params)
+            .query(&query_params_2)
             .send()
-            .await
-            .expect("Error while trying to response from get_followers_by_id endpoint");
+            .await {
+                Ok(resp) => resp,
+                Err(e) => return Err(e),
+            };
         let resp_textified = user_request.text().await.expect("Error while trying to textify response");
         let resp_jsonified: Value = serde_json::from_str(&resp_textified).expect("Error while trying to jsonify response(already textified)");
         Ok(resp_jsonified)
@@ -30,6 +59,7 @@ mod tests {
         client.authenticate().await;
         let params = PGetFollowersByIdBuilder::default().id(id.clone()).build()?;
         let resp = client.get_followers_by_id(&params).await;
+        println!("{:?}", resp);
         assert!(resp.is_ok());
         Ok(())
     }
--- src/api/users/follows/params_structs.rs
+++ src/api/users/follows/params_structs.rs
@@ -5,4 +5,20 @@ use serde::{Serialize, Deserialize};
 #[derive(Builder, Default, Debug)]
 pub struct PGetFollowersById {
     pub id: String,
+    #[builder(setter(strip_option), default)]
+    pub max_results: Option<i32>,
+    #[builder(setter(strip_option), default)]
+    pub pagination_token: Option<String>,
+    #[builder(setter(strip_option), default)]
+    pub expansions: Option<Vec<String>>,
+    #[builder(setter(strip_option), default)]
+    pub tweet_fields: Option<Vec<String>>,
+    #[builder(setter(strip_option), default)]
+    pub user_fields: Option<Vec<String>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QPGetFollowersById {
+    pub max_results: i32,
+    pub expansions: String,
 }
